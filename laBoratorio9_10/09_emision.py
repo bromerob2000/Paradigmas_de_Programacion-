d9f50aa2c5659cf57817b9a50472bd5a064958cd
IvanMoralesR <ivsaimortius@gmail.com>
2023-06-30 13:52:46 -0600

    Video 9 y parte del 10

---
+++ laBoratorio9_10/09_emision.py
@@ -0,0 +1,33 @@
+#-------------------------------
+#       Broadcast con MPI
+#     distribución de datos
+#-------------------------------
+
+from mpi4py import MPI
+
+# objeto cominicador
+comm = MPI.COMM_WORLD
+
+# quien soy
+rank = comm.Get_rank()
+
+#-------------------------------
+#    El proceso 0 tiene datos
+#        y los otros no
+#-------------------------------
+
+if rank == 0:
+    data = {'key1': [7,2.72,2+3],
+            'key2': ('abc','xyz')}
+else:
+    data = None
+
+#-------------------------------
+# Envíar diccionario a todos los
+#    procesos desde root
+#-------------------------------
+
+data = comm.bcast(data, root=0)
+print(data)
+
+
